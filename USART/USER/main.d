b77a2b403f7c788238651e75ec39f75d07738a7f
wuxiaojiang <717189544@qq.com>
2015-04-26 16:37:29 +0800

    holmes

---
+++ USART/USER/main.d
@@ -0,0 +1,11 @@
+main.o: main.c
+main.o: stm32f10x.h
+main.o: ..\CORE\core_cm3.h
+main.o: E:\keil4\ARM\ARMCC\bin\..\include\stdint.h
+main.o: system_stm32f10x.h
+main.o: stm32f10x_conf.h
+main.o: ..\STM32_FWLIB\inc\stm32f10x_gpio.h
+main.o: ..\USER\stm32f10x.h
+main.o: ..\STM32_FWLIB\inc\stm32f10x_rcc.h
+main.o: ..\STM32_FWLIB\inc\stm32f10x_usart.h
+main.o: E:\keil4\ARM\ARMCC\bin\..\include\stdio.h
---
+++ USART/USER/stm32f10x_gpio.d
@@ -0,0 +1,11 @@
+.\stm32f10x_gpio.o: ..\STM32_FWLIB\src\stm32f10x_gpio.c
+.\stm32f10x_gpio.o: ..\STM32_FWLIB\inc\stm32f10x_gpio.h
+.\stm32f10x_gpio.o: ..\USER\stm32f10x.h
+.\stm32f10x_gpio.o: ..\CORE\core_cm3.h
+.\stm32f10x_gpio.o: E:\keil4\ARM\ARMCC\bin\..\include\stdint.h
+.\stm32f10x_gpio.o: ..\USER\system_stm32f10x.h
+.\stm32f10x_gpio.o: ..\USER\stm32f10x_conf.h
+.\stm32f10x_gpio.o: ..\STM32_FWLIB\inc\stm32f10x_gpio.h
+.\stm32f10x_gpio.o: ..\STM32_FWLIB\inc\stm32f10x_rcc.h
+.\stm32f10x_gpio.o: ..\USER\stm32f10x.h
+.\stm32f10x_gpio.o: ..\STM32_FWLIB\inc\stm32f10x_usart.h
